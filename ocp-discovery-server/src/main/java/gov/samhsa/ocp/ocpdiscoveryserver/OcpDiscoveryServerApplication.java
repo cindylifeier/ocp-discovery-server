e87fc57d7c4f567a8ad003f0352207c553eddbae
sadhch <sadhana.chandravanka@feisystems.com>
2018-01-17 12:26:32 -0500

    Create a Spring Boot Project with Eureka Server and Actuator Dependencies
    #7349 - As a development team member, I can rely on development process environment and application infrastructure services for working software fast delivery

---
+++ ocp-discovery-server/src/main/java/gov/samhsa/ocp/ocpdiscoveryserver/OcpDiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.ocp.ocpdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpDiscoveryServerApplication.class, args);
+	}
+}
